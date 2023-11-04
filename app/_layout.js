c78231f3b00b6709bd915e25c25d1fd4ffb535cd
bouncingbumble <jordboudreau@gmail.com>
2023-11-03 17:26:49 -0700

    status bar customization

--- app/_layout.js
+++ app/_layout.js
@@ -2,52 +2,67 @@ import { Tabs } from 'expo-router/tabs'
 import GridIcon from '../assets/GridIcon'
 import ListIcon from '../assets/ListIcon'
 import MapIcon from '../assets/MapIcon'
+import { useFonts } from 'expo-font'
+import GeneralStatusBarColor from '../Components/GeneralStatusBarColor'
 
 export default function AppLayout() {
+    const [fontsLoaded] = useFonts({
+        'Nanum Gothic': require('../assets/fonts/NanumGothic-Regular.ttf'),
+        'Nanum Gothic Bold': require('../assets/fonts/NanumGothic-Bold.ttf'),
+    })
+
     return (
-        <Tabs
-            screenOptions={{
-                tabBarStyle: {
-                    backgroundColor: '#a42229',
-                    paddingVertical: 16,
-                },
-            }}
-        >
-            <Tabs.Screen
-                // Name of the route to hide.
-                name="index"
-                options={{
-                    // This tab will no longer show up in the tab bar.
-                    href: null,
-                }}
-            />
-            <Tabs.Screen
-                name="grid"
-                options={{
-                    headerShown: false,
-                    tabBarLabel: '',
-                    tabBarIcon: () => <GridIcon />,
-                    tabBarIconStyle: { marginBottom: -8 },
-                }}
-            />
-            <Tabs.Screen
-                name="list"
-                options={{
-                    headerShown: false,
-                    tabBarLabel: '',
-                    tabBarIcon: () => <ListIcon />,
-                    tabBarIconStyle: { marginBottom: -8 },
-                }}
-            />
-            <Tabs.Screen
-                name="map"
-                options={{
-                    headerShown: false,
-                    tabBarLabel: '',
-                    tabBarIcon: () => <MapIcon />,
-                    tabBarIconStyle: { marginBottom: -8 },
-                }}
-            />
-        </Tabs>
+        fontsLoaded && (
+            <>
+                <GeneralStatusBarColor
+                    backgroundColor="#a42229"
+                    barStyle="light-content"
+                />
+                <Tabs
+                    screenOptions={{
+                        tabBarStyle: {
+                            backgroundColor: '#a42229',
+                            paddingVertical: 16,
+                        },
+                    }}
+                >
+                    <Tabs.Screen
+                        // Name of the route to hide.
+                        name="index"
+                        options={{
+                            // This tab will no longer show up in the tab bar.
+                            href: null,
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="grid"
+                        options={{
+                            headerShown: false,
+                            tabBarLabel: '',
+                            tabBarIcon: () => <GridIcon />,
+                            tabBarIconStyle: { marginBottom: -8 },
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="list"
+                        options={{
+                            headerShown: false,
+                            tabBarLabel: '',
+                            tabBarIcon: () => <ListIcon />,
+                            tabBarIconStyle: { marginBottom: -8 },
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="map"
+                        options={{
+                            headerShown: false,
+                            tabBarLabel: '',
+                            tabBarIcon: () => <MapIcon />,
+                            tabBarIconStyle: { marginBottom: -8 },
+                        }}
+                    />
+                </Tabs>
+            </>
+        )
     )
 }
--- app/list.js
+++ app/list.js
@@ -5,20 +5,77 @@ import {
     StyleSheet,
     View,
     Pressable,
+    ImageBackground,
+    StatusBar,
 } from 'react-native'
 import pizzaPlaces from '../assets/pizza-places.json'
+import CompassIcon from '../assets/CompassIcon'
+import GeneralStatusBarColor from '../Components/GeneralStatusBarColor'
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    pic: {
+        flex: 1,
+        height: 200,
+        width: '100%',
+        marginTop: 12,
+        marginBottom: 12,
+    },
+    item: {
+        flex: 1,
+        padding: 12,
+    },
+    card: {},
+    name: {
+        fontSize: 20,
+        color: '#000',
+        fontFamily: 'Nanum Gothic Bold',
+    },
+    addressContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    address: {
+        color: '#f1af4d',
+        marginLeft: 8,
+        fontFamily: 'Nanum Gothic Bold',
+    },
+    mapIcon: {
+        height: 24,
+        width: 24,
+        backgroundColor: '#a42229',
+        borderRadius: '23%',
+    },
+    separator: {
+        backgroundColor: '#316134',
+        height: 6,
+    },
 })
 
 const DATA = pizzaPlaces
 
-const Item = ({ name }) => (
+const Item = ({ name, imageUrl, address }) => (
     <View style={styles.item}>
         <Text style={styles.name}>{name}</Text>
+        <View style={styles.card}>
+            <ImageBackground
+                source={{
+                    uri: imageUrl,
+                }}
+                resizeMode="cover"
+                style={styles.pic}
+                borderRadius={16}
+            ></ImageBackground>
+            <View style={styles.addressContainer}>
+                <View style={styles.mapIcon}>
+                    <CompassIcon />
+                </View>
+                <Text style={styles.address}>{address}</Text>
+            </View>
+        </View>
     </View>
 )
 
@@ -26,16 +83,22 @@ export default function List() {
     const onPressFunction = () => {
         alert('hello')
     }
+
     return (
         <SafeAreaView style={styles.container}>
             <FlatList
                 data={DATA}
                 renderItem={({ item }) => (
                     <Pressable onPress={onPressFunction}>
-                        <Item name={item.name} />
+                        <Item
+                            name={item.name}
+                            imageUrl={item.image_url}
+                            address={item.location.address1}
+                        />
                     </Pressable>
                 )}
                 keyExtractor={(item) => item.id}
+                ItemSeparatorComponent={<View style={styles.separator} />}
             />
         </SafeAreaView>
     )
